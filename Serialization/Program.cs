096ddc5dbd8ba06052850a4813205c0144f16ab1
Hyunseung Choi <bocalist@gmail.com>
2024-03-12 09:03:16 -0600

    Using BinaryFormatter to serialize object and save it into a file

---
+++ Serialization/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Serialization
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Student s = new Student()
+            {
+                Name = "Hyunseung",
+            };
+
+            string path = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
+
+            Console.WriteLine(path);
+            string binPath = path + "/student.bin";
+
+            SerializeStudent(binPath, s);
+        }
+
+        public static void SerializeStudent(string path, Student s)
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            using (FileStream fs = new FileStream(path, FileMode.Create))
+            {
+                formatter.Serialize(fs, s);
+            }
+            Console.WriteLine("Binary Serialization Completed");
+        }
+    }
+}
