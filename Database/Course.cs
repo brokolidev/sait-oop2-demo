0b67482b9ef65d27a957447f65175e10fbb6a256
brokoli <bocalist@gmail.com>
2024-04-02 22:36:07 -0600

    set properties for course class

--- Database/Course.cs
+++ Database/Course.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,23 @@ namespace Database
 {
     internal class Course
     {
+        public int CourseId { get; set; }
+        public string Name { get; set; }
+        public int Credits { get; set; }
+
+
+        // basic constroctor
+        public Course()
+        {
+        }
+
+        public override string ToString()
+        {
+            return $"Coures ID : {CourseId}\n" +
+                $"Name : {Name}\n" +
+                $"Credits : {Credits}";
+        }
+
+
     }
 }
